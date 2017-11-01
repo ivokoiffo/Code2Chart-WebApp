08fab8742a1305bd2a2981acc6d229a191964169
ivokoiffo <ivan_superi@hotmail.com>
2017-11-01 20:12:47 -0300

    Agrego cambio en estilo

--- src/main/resources/static/app/app.module.js
+++ src/main/resources/static/app/app.module.js
@@ -4,7 +4,19 @@
     // Creating our angular app and inject ui-router 
     var app = angular.module('code2chart', ['ui.router','ngMaterial','ngFileUpload','toaster'])
     			
+    app.config(function($mdThemingProvider) {
 
+    	  $mdThemingProvider.theme('default')
+    	    .primaryPalette('grey', {
+    	      'default': '400', // by default use shade 400 from the pink palette for primary intentions
+    	    })
+    	    // If you specify less than all of the keys, it will inherit from the
+    	    // default shades
+    	    .accentPalette('orange', {
+    	      'default': '600' // use shade 200 for default, and keep all other shades the same
+    	    });
+
+    	});
     // Configuring our states, each one of these is a new stage in the process
     app.config(['$stateProvider', '$urlRouterProvider',
 
