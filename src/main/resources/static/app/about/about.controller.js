ba8f0b12a59b7da473addd4abdd4d4bc1e8b364b
ivokoiffo <ivan_superi@hotmail.com>
2017-11-03 20:44:58 -0300

    Cambios de diseño

--- src/main/resources/static/app/about/about.controller.js
+++ src/main/resources/static/app/about/about.controller.js
@@ -24,24 +24,26 @@
     		);
     	};
     	
-    	function Person (fullname,age,bio,ruta){
+    	function Person (fullname,age,bio,minibio,ruta,rutaLinkedin,rutaGithub){
     		    this.name = fullname;
     		    this.age = age;
     		    this.bio = bio;
+    		    this.minibio = minibio;
     		    this.ruta = ruta;
+    		    this.rutaLinkedin= rutaLinkedin;
+    		    this.rutaGithub=rutaGithub;
     		};
     	
     	$scope.team = [];
-    	var rodri = new Person("Rodrigo Vazquez",22,"Universidad Tecnologica Nacional - FRBA" +
-    			"Trabaja en Ford Motor Company","content/images/rodrigo.png");
-    	var koiffo = new Person("Iván Koiffman",22,"Universidad Tecnologica Nacional - FRBA" +
-    			"Trabaja en Base4Security","content/images/ivan.png");
-    	var santi = new Person("Santiago Varela",23,"Universidad Tecnologica Nacional - FRBA" +
-    			"Trabaja en Siemens","content/images/santiago.png");
-    	var jony = new Person("Jonatan Martinez",22,"Universidad Tecnologica Nacional - FRBA" +
-    			"Trabaja en EY","content/images/rodrigo.png");
-    	var nico = new Person("Nicolas Taccari",22,"Universidad Tecnologica Nacional - FRBA" +
-    			"Trabaja en ExxonMobil","content/images/rodrigo.png");
+    	var rodri = new Person("Rodrigo Vazquez",22,"","Trabaja en Ford Motor Company","content/images/rodrigo.png","https://www.linkedin.com/in/rodrigo-vazquez-102b2a114/","https://github.com/rovazquez");
+    	var koiffo = new Person("Iván Koiffman",22,"Me intereso en mejorar la seguridad de un sistema a través de la identificación y explotación de vulnerabilidades."
+    			,"Trabaja en Base4Security","content/images/ivan.png","https://www.linkedin.com/in/ivankoiffman/","https://github.com/ivokoiffo");
+    	var santi = new Person("Santiago Varela",23,"",
+    			"Trabaja en Siemens","content/images/santiago.png","https://www.linkedin.com/in/santiago-varela-379658a9/","https://github.com/santiagojvarela");
+    	var jony = new Person("Jonatan Martinez",22,"",
+    			"Trabaja en EY","content/images/rodrigo.png","https://www.linkedin.com/in/jonatan-gabriel-martinez-47a47093/","https://github.com/jonmartinez123");
+    	var nico = new Person("Nicolas Taccari",22,"",
+    			"Trabaja en ExxonMobil","content/images/rodrigo.png","https://www.linkedin.com/in/nicolas-taccari-84a296139/","https://github.com/nicoTaccari");
     	
     	$scope.team.push(rodri);
     	$scope.team.push(koiffo);
