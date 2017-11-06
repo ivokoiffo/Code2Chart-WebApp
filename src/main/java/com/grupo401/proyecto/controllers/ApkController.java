40d7f8188e0d1546a00528449a7fc9866c0d845f
Nicolas Taccari <nicolastaccari@gmail.com>
2017-11-06 16:04:23 -0300

    fix el drive ahora toma del server el file

--- src/main/java/com/grupo401/proyecto/controllers/ApkController.java
+++ src/main/java/com/grupo401/proyecto/controllers/ApkController.java
@@ -1,12 +1,7 @@
 package com.grupo401.proyecto.controllers;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,10 +20,6 @@ public class ApkController {
 		try {
 			
 			InputStream inputStream = ClassLoader.getSystemResourceAsStream("test.apk");
-			/*File apk = new File("test.apk");
-			String path = apk.getAbsolutePath();
-			Path p = FileSystems.getDefault().getPath(path);
-			byte[] bytes2 = Files.readAllBytes(p);*/
 			return IOUtils.toByteArray(inputStream);
 		} catch (Exception e) {
 			return null;
